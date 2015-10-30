c7ad3467ca82af204f243954943e258ecd10b7f8
Timothy Pratley <timothypratley@gmail.com>
2015-10-29 20:23:32 -0700

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject timothypratley/reanimated "0.1.1"
+(defproject timothypratley/reanimated "0.1.2-SNAPSHOT"
   :description "Reanimated is an animation library for Reagent (ClojureScript)."
   :url "http://github.com/timothypratley/reanimated"
   :license {:name "Eclipse Public License"
