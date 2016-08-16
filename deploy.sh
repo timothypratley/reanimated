2439352d688dff4b83e125c9210a53c730149ac5
Timothy Pratley <timothypratley@gmail.com>
2016-08-15 19:49:20 -0700

    Hmmm the other deploy method doesn't work for me

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+cd $(dirname $0)
+cd public
+git init
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "git@github.com:timothypratley/reanimated.git" master:gh-pages
+rm -fr resources/public/.git
