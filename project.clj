337926deda118bdffae1e10c16d3a1be84ddf0ab
Timothy Pratley <timothypratley@gmail.com>
2018-03-16 04:37:48 -0700

    updates project file to be in sync with boot file

--- project.clj
+++ project.clj
@@ -1,26 +1,23 @@
 (defproject
   reanimated
-  "0.4.0"
+  "0.6.1"
   :comment
   "Generated from build.boot for Cursive"
   :dependencies
   [[org.clojure/clojure "1.9.0"]
-   [org.clojure/clojurescript "1.9.89"]
-   [boot-codox "0.9.5" :scope "test"]
-   [pandeiro/boot-http "0.7.3" :scope "test"]
-   [adzerk/boot-reload "0.4.12" :scope "test"]
-   [adzerk/boot-cljs "1.7.228-1" :scope "test"]
+   [org.clojure/clojurescript "1.9.908"]
+   [boot-codox "0.10.3" :scope "test"]
+   [pandeiro/boot-http "0.8.3" :scope "test"]
+   [adzerk/boot-reload "0.5.2" :scope "test"]
+   [adzerk/boot-cljs "2.1.4" :scope "test"]
    [adzerk/bootlaces "0.1.13" :scope "test"]
    [adzerk/boot-cljs-repl "0.3.3" :scope "test"]
-   [com.cemerick/piggieback "0.2.1" :scope "test"]
+   [com.cemerick/piggieback "0.2.2" :scope "test"]
    [weasel "0.7.0" :scope "test"]
-   [org.clojure/tools.nrepl "0.2.12" :scope "test"]
-   [reagent "0.6.0-rc"]
-   [devcards "0.2.1-7" [cljsjs/react-dom cljsjs/react]]
-   [data-frisk-reagent "0.2.5"]
-   [fipp "0.6.6"]]
+   [org.clojure/tools.nrepl "0.2.13" :scope "test"]
+   [reagent "0.7.0"]]
   :repositories
-  [["clojars" {:url "https://clojars.org/repo/"}]
+  [["clojars" {:url "https://repo.clojars.org/"}]
    ["maven-central" {:url "https://repo1.maven.org/maven2"}]]
   :source-paths
   ["src"])
